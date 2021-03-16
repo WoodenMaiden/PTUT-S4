6204fd968d2a680b98a6625a9287b88e47dba7e7
Artho-dev <loic.ganne@etu.univ-amu.fr>
2021-03-16 10:37:27 +0100

    Remise en place du main

--- src/fr/univ_amu/DumbStages/Main.java
+++ src/fr/univ_amu/DumbStages/Main.java
@@ -1,13 +1,8 @@
 package fr.univ_amu.DumbStages;
 
-import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
-
-import java.io.IOException;
-
 //Classe servant uniquement à lancer l'Application
 public class Main {
-    public static void main(String[] args) throws IOException, InvalidFormatException {
-        //App.launch(App.class, args);
-        LecteurOral.Start();
+    public static void main(String[] args) {
+        App.launch(App.class, args);
     }
 }
